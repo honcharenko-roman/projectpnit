d07788d8f6f17f8e513d487651326994eb27a25f
yunhaveto <yunhaveto@gmail.com>
2019-03-22 11:58:51 +0200

    still adopting UI

--- app/src/main/java/com/example/yun/myapplication/SingInActivity.java
+++ app/src/main/java/com/example/yun/myapplication/SingInActivity.java
@@ -2,8 +2,10 @@ package com.example.yun.myapplication;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -18,7 +20,28 @@ public class SingInActivity extends AppCompatActivity {
         Typeface font = Typeface.createFromAsset(getAssets(), "Montserrat-Black.ttf");
         ((TextView)findViewById(R.id.signInText)).setTypeface(font);
 
-        ImageView imageView = findViewById(R.id.topPanelImageView);
-        imageView.getLayoutParams().height = 132;
+        font = Typeface.createFromAsset(getAssets(), "Montserrat-Italic.ttf");
+        ((TextView)findViewById(R.id.forgotPasswordText)).setTypeface(font);
+        ((TextView)findViewById(R.id.createAccountText)).setTypeface(font);
+        ((EditText)findViewById(R.id.passEditText)).setTypeface(font);
+        ((EditText)findViewById(R.id.emailEditText)).setTypeface(font);
+
+        //Displaying TextInputLayout Error
+        /*TextInputLayout emailLayout = (TextInputLayout) findViewById(R.id
+                .fEmailLayout);
+        emailLayout.setErrorEnabled(true);
+        emailLayout.setError("Min 2 chars required");*/
+
+        //Displaying EditText Error
+        /*EditText age = (EditText) findViewById(R.id.emailEditText);
+        age.setError("Required");*/
+
+        //Displaying both TextInputLayout and EditText Errors
+        /*TextInputLayout passLayout = (TextInputLayout) findViewById(R.id
+                .fPassLayout);
+        passLayout.setErrorEnabled(true);
+        passLayout.setError("Please enter a phone number");
+        EditText phone = (EditText) findViewById(R.id.passEditText);
+        phone.setError("Required");*/
     }
 }
