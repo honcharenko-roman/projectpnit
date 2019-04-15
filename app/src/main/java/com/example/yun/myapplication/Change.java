27598f4ab33242a430476de9d92b8162419686b4
yunhaveto <yunhaveto@gmail.com>
2019-04-15 21:09:19 +0300

    configuring retrofit

---
+++ app/src/main/java/com/example/yun/myapplication/Change.java
@@ -0,0 +1,13 @@
+package com.example.yun.myapplication;
+
+public class Change {
+    String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
