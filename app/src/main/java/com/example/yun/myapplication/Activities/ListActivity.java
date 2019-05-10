b8c5e024c18c0a0812a84cc99fecec444155fae1
Illia Uzun <uzun.illia.main@gmail.com>
2019-05-10 21:34:43 +0300

    navigation drawer fix

--- app/src/main/java/com/example/yun/myapplication/Activities/ListActivity.java
+++ app/src/main/java/com/example/yun/myapplication/Activities/ListActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.example.yun.myapplication.Entities.Medic;
@@ -35,6 +38,8 @@ public class ListActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private Button mToggle;
 
+    public static int navItemIndex = 0;
+
     private NavigationView mNavigationView;
 
     @Override
@@ -50,7 +55,7 @@ public class ListActivity extends AppCompatActivity {
             mDrawerLayout.openDrawer(mNavigationView);
         });
 
-
+        setUpNavigationView();
 
         mMedicList = null;
 
@@ -69,31 +74,14 @@ public class ListActivity extends AppCompatActivity {
                         t.printStackTrace();
                     }
                 });
+
     }
 
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (mToggleButton.isActivated()){
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-    /*public void insertItem(int position) {
-        mMedicList.add(position, new Medic(R.drawable.ic_android, "New Item At Position" + position, "This is Line 2"));
-        mAdapter.notifyItemInserted(position);
-    }*/
 
     public void removeItem(int position) {
         mMedicList.remove(position);
         mAdapter.notifyItemRemoved(position);
     }
-
-    /*public void changeItem(int position, String text) {
-        mMedicList.get(position).changeText1(text);
-        mAdapter.notifyItemChanged(position);
-    }*/
 
     public void buildRecyclerView() {
         mRecyclerView = findViewById(R.id.recyclerView);
@@ -102,31 +90,6 @@ public class ListActivity extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-
-        /*mAdapter.setOnItemClickListener(position -> {
-            Intent intent = new Intent(this, MedProfileActivity.class);
-            intent.putExtra("position", position);
-            startActivity(intent);
-        });*/
-
-
-        findViewById(R.id.listClearDataBaseButton).setOnClickListener(v->{
-            NetworkService
-                    .getInstance()
-                    .getJSONApi()
-                    .deleteMedics()
-                    .enqueue(new Callback<Medic>() {
-                        @Override
-                        public void onResponse(Call<Medic> call, Response<Medic> response) {
-
-                        }
-
-                        @Override
-                        public void onFailure(Call<Medic> call, Throwable t) {
-
-                        }
-                    });
-        });
 
         mAdapter.setOnItemClickListener(new MedicAdapter.OnItemClickListener() {
             @Override
@@ -142,9 +105,101 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.listContinueButton).setOnClickListener(v->{
+        findViewById(R.id.listContinueButton).setOnClickListener(v -> {
             Intent intent = new Intent(ListActivity.this, SingInActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawers();
+            return;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.map) {
+            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        // user is in notifications fragment
+        // and selected 'Mark all as Read'
+        if (id == R.id.List) {
+            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
+        }
+
+        // user is in notifications fragment
+        // and selected 'Clear All'
+        if (id == R.id.Profile) {
+            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void selectNavMenu() {
+        mNavigationView.getMenu().getItem(1).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.List:
+                        navItemIndex = 0;
+                        break;
+                    case R.id.Profile:
+                        navItemIndex = 1;
+                        break;
+                    case R.id.favourites:
+                        navItemIndex = 2;
+                        break;
+                    case R.id.premium:
+                        navItemIndex = 3;
+                        break;
+                    case R.id.map:
+                        startActivity(new Intent(ListActivity.this, MapActivity.class));
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.settings:
+//                        startActivity(new Intent(ListActivity.this, AboutUsActivity.class));
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.signIn:
+                        startActivity(new Intent(ListActivity.this, SingInActivity.class));
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+
+                return true;
+            }
+        });
+
     }
 }
