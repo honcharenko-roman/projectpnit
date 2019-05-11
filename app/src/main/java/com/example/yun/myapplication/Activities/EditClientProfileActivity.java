093375fae494dd3690ed3783780da6afadb21b7c
Illia Uzun <uzun.illia.main@gmail.com>
2019-05-11 03:30:06 +0300

    client scroll

--- app/src/main/java/com/example/yun/myapplication/Activities/EditClientProfileActivity.java
+++ app/src/main/java/com/example/yun/myapplication/Activities/EditClientProfileActivity.java
@@ -1,6 +1,5 @@
 package com.example.yun.myapplication.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +7,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.yun.myapplication.Entities.Account;
-import com.example.yun.myapplication.Entities.Medic;
 import com.example.yun.myapplication.R;
 import com.example.yun.myapplication.Retrofit.NetworkService;
 
@@ -29,7 +27,7 @@ public class EditClientProfileActivity extends AppCompatActivity {
         EditText infoField = findViewById(R.id.editClientProfileInformationField);
         EditText telephoneField = findViewById(R.id.editClientProfilePhoneNumberField);
 
-        findViewById(R.id.editClientProfileSendButton).setOnClickListener(v -> {
+        findViewById(R.id.sendButton).setOnClickListener(v -> {
 
             Account account = new Account(
                     nameField.getText().toString(),
@@ -41,8 +39,8 @@ public class EditClientProfileActivity extends AppCompatActivity {
                     addressField.getText().toString()
             );
 
-            TextView textView = findViewById(R.id.editClientProfileText);
-            textView.setText(nameField.getText());
+//            TextView textView = findViewById(R.id.editClientProfileText);
+//            textView.setText(nameField.getText());
 
             Call<Account> call = NetworkService
                     .getInstance().getJSONApi()
--- app/src/main/java/com/example/yun/myapplication/Activities/EditProfileActivity.java
+++ app/src/main/java/com/example/yun/myapplication/Activities/EditProfileActivity.java
@@ -1,22 +1,17 @@
 package com.example.yun.myapplication.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.example.yun.myapplication.Entities.Medic;
 import com.example.yun.myapplication.R;
 import com.example.yun.myapplication.Retrofit.NetworkService;
-
-import org.w3c.dom.Text;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,12 +26,12 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        EditText nameField  = findViewById(R.id.editProfileNameField);
-        EditText surnameField  = findViewById(R.id.editProfileSurnameField);
-        EditText addressField  = findViewById(R.id.editProfileAddressField);
-        EditText infoField  = findViewById(R.id.editProfileInformationField);
+        EditText nameField  = findViewById(R.id.editClientProfileNameField);
+        EditText surnameField  = findViewById(R.id.editClientProfileSurnameField);
+        EditText addressField  = findViewById(R.id.editClientProfileAddressField);
+        EditText infoField  = findViewById(R.id.editClientProfileInformationField);
         EditText experienceField  = findViewById(R.id.editProfileExperienceField);
-        EditText telephoneField  = findViewById(R.id.editProfilePhoneNumberField);
+        EditText telephoneField  = findViewById(R.id.editClientProfilePhoneNumberField);
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
         mySpinner.setAdapter(new ArrayAdapter<YELL.main.domain.Categories>(this, android.R.layout.simple_spinner_item, YELL.main.domain.Categories.values()));
